f8285911f55266222ddb37c3d0b7b72275d446bb
sinda <bensalahsinda14@gmail.com>
2025-12-01 03:50:36 +0100

    Refactor SnakeGame to include level and difficulty

--- src/main/java/com/devops/game/SnakeGame.java
+++ src/main/java/com/devops/game/SnakeGame.java
@@ -1,56 +1,88 @@
 package com.devops.game;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class SnakeGame {
-    private List<Position> snake;
-    private Position food;
+    private int width;
+    private int height;
     private int score;
-    private boolean gameOver;
+    private int level;
+    private String difficulty;
     
     public static class Position {
-        int x, y;
+        public int x;
+        public int y;
+        
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
         }
-        public int getX() { return x; }
-        public int getY() { return y; }
-    }
-    
-    public SnakeGame() {
-        snake = new ArrayList<>();
-        snake.add(new Position(10, 10));
-        snake.add(new Position(9, 10));
-        snake.add(new Position(8, 10));
-        score = 0;
-        gameOver = false;
-        food = new Position(15, 15);
-    }
-    
-    public List<Position> getSnake() { return snake; }
-    public Position getFood() { return food; }
-    public int getScore() { return score; }
-    public boolean isGameOver() { return gameOver; }
-    
-    public void move() {
-        if (!gameOver) {
-            Position head = snake.get(0);
-            Position newHead = new Position(head.x + 1, head.y);
-            snake.add(0, newHead);
-            
-            if (newHead.x == food.x && newHead.y == food.y) {
-                score += 10;
-                food = new Position(new Random().nextInt(20), new Random().nextInt(20));
-            } else {
-                snake.remove(snake.size() - 1);
-            }
-            
-            if (newHead.x < 0 || newHead.x >= 20 || newHead.y < 0 || newHead.y >= 20) {
-                gameOver = true;
-            }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Position position = (Position) obj;
+            return x == position.x && y == position.y;
         }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+    
+    public SnakeGame(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.score = 0;
+        this.level = 1;
+        this.difficulty = "medium";
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+    public String getDifficulty() {
+        return difficulty;
+    }
+    
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    
+    public int getSpeedByDifficulty() {
+        switch(difficulty) {
+            case "easy": return 200;
+            case "medium": return 150;
+            case "hard": return 100;
+            case "expert": return 60;
+            default: return 150;
+        }
+    }
+    
+    public int calculateLevelSpeed(int baseSpeed) {
+        return Math.max(50, baseSpeed - (level * 10));
     }
 }
